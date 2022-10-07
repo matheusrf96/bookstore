afb8331821826091364ad2d0b7f1fea2d3621e24
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-07 12:55:09 -0300

    feat (server): Create basic routes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require("http")
 const port = 3000
 
+const routes = {
+    '/': 'Homepage',
+    '/books': 'Books list',
+    '/authors': 'Authors list',
+    '/about': 'Project info',
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Node bookstore')
+    res.end(routes[req.url])
 })
 
 server.listen(port, () => console.log(`Running on port ${port}`))
