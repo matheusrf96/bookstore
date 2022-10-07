e516c34656af7f90ee39e5764a5244a2e0aeb97a
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-07 12:44:43 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http")
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Node bookstore')
+})
+
+server.listen(port, () => console.log(`Running on port ${port}`))
